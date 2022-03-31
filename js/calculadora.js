8e7f4a93272081c527528155ea3868c48979924c
Alejo Samardich <alejon100@gmail.com>
2022-03-30 23:18:31 -0300

    JS

---
+++ js/calculadora.js
@@ -0,0 +1,13 @@
+(function () {
+    const list1 = document.getElementById('list_1');
+    const list2 = document.getElementById('list_2');
+    const onChange = (e) => {
+      let result = document.getElementById('result');
+      result.innerHTML = '';
+      if (list1.value && list2.value)
+        result.innerHTML = parseInt(list1.value) + parseInt(list2.value) + "$";
+    };
+
+    list1.addEventListener('change', onChange);
+    list2.addEventListener('change', onChange);
+  })();
